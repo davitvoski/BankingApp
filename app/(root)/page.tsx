9346c39517576dd8b8bab77df6b8c47fcca73c8b
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2024-05-03 17:24:58 -0400

    Implement total current balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
-}
+  const loggedIn = { firstName: "Davit" };
 
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.31} />
+        </header>
+      </div>
+    </section>
+  );
+}
 export default Home;
