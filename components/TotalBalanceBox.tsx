3b65fa33b0e8b4e0e9697f4524487faac21b98ef
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2024-05-05 16:01:59 -0400

    Fixe chart layout css

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -2,19 +2,22 @@ import { formatAmount } from "@/lib/utils";
 import React from "react";
 import CountUp from "react-countup";
 import AnimatedCounter from "./AnimatedCounter";
+import DoughnutChart from "./DoughnutChart";
 
 const TotalBalanceBox = ({ accounts = [], totalBanks, totalCurrentBalance }: TotlaBalanceBoxProps) => {
   return (
     <section className="total-balance">
-      <div className="total-balance-chart"></div>
-        {/* DoughnutChart */}
+      <div className="total-balance-chart">
+        <DoughnutChart accounts={accounts} />
+      </div>
+
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
         <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
-          <p className="total-balance-amount flex-center gap-2">
+          <div className="total-balance-amount flex-center gap-2">
             <AnimatedCounter amount={totalCurrentBalance} />
-          </p>
+          </div>
         </div>
       </div>
     </section>
